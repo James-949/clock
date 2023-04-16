1865a6f7b77d3e9a8c313146dcc6daa05fc0c0ff
James-949 <130909119+James-949@users.noreply.github.com>
2023-04-16 06:07:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿var th = 0;
+var dq = 0;
+var sa = 0;
+var i = 0;
+while (i < 1) {
+    i++;
+    document.getElementById("sa").innerHTML = sa;
+    document.getElementById("dq").innerHTML = dq;
+    document.getElementById("th").innerHTML = th;
+    if (th = 576) {
+        th = 0;
+        dq = 1;
+    } else if (dq = 30) {
+        dq = 0;
+        sa = 1;
+    } else if (sa = 5) {
+        sa = 0;
+    }
+}
